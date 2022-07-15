4c742dea7e6dbe4d39e31e886742f5f458f215ff
igorplopes <igorp.lopes16@gmail.com>
2022-07-14 23:42:26 -0300

    feat: add method to retrieve a list of pages from a pdf

--- src/pdfParserService/pdfParserService.py
+++ src/pdfParserService/pdfParserService.py
@@ -3,6 +3,10 @@ import fitz
 def open_pdf(file_path):
     return fitz.open(file_path)
 
+def get_pdf_pages(pdf):
+    total_pages = pdf.page_count
+    return [pdf.load_page(i) for i in range(total_pages)]
+
 def extract_blocks_from_page(page):
     return page.get_text("blocks", sort=True)
 
