e74725d33b7e94abf0766951bde88b10392ff46b
igorplopes <igorp.lopes16@gmail.com>
2022-07-16 22:39:43 -0300

    refactor: adjust module and package naming

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from core.directory_utils import get_input_files
-from pdfParserService.pdfParserService import open_pdf
+from business.pdf_service import open_pdf
+from business.xp_service import extract_data_from_xp_pdf
 
 
 def notas_corretagem_parser():
     files = get_input_files()
     for file in files:
-        open_pdf()
+        current_pdf = open_pdf(file)
+        extract_data_from_xp_pdf(current_pdf)
 
 
 if __name__ == "__main__":
