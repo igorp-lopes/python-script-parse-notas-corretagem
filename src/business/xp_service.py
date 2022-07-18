924d9a5be21f25a52d460d1c9a5644bbf0c2dfa8
igorplopes <igorp.lopes16@gmail.com>
2022-07-17 21:48:22 -0300

    feat: add method to extract date from xp pdf

--- src/business/xp_service.py
+++ src/business/xp_service.py
@@ -1,3 +1,13 @@
+from business.pdf_service import extract_text_from_block
+from core.utils import extract_date_from_string
+
+
 def is_xp_pdf(pdf):
     first_page_text = pdf.get_page_text(0)
     return "XP INVESTIMENTOS" in first_page_text
+
+
+def get_operation_date(blocks):
+    date_block = list(
+        filter(lambda block: extract_date_from_string(extract_text_from_block(block)) is not None, blocks))
+    return extract_date_from_string(date_block[1])
