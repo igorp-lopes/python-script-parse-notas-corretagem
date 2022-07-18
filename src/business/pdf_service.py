8f6d34e75c4df17c49ade85e61bc5aaa8360116b
igorplopes <igorp.lopes16@gmail.com>
2022-07-17 23:46:50 -0300

    refactor: optimize function to extract text from blocks to use list comprehensions

--- src/business/pdf_service.py
+++ src/business/pdf_service.py
@@ -1,11 +1,14 @@
 import fitz
 
+
 def open_pdf(file_path):
     return fitz.open(file_path)
 
+
 def get_pdf_pages(pdf):
     total_pages = pdf.page_count
-    return [pdf.load_page(i) for i in range(total_pages)]
+    return (pdf.load_page(i) for i in range(total_pages))
+
 
 def extract_blocks_from_page(page):
     return page.get_text("blocks", sort=True)
@@ -16,9 +19,4 @@ def extract_text_from_block(block):
 
 
 def extract_text_from_blocks(blocks):
-    text = []
-
-    for block in blocks:
-        text.append(extract_text_from_block(block))
-
-    return text
+    return [extract_text_from_block(block) for block in blocks]
