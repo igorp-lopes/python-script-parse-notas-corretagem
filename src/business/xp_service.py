cc4c806587938dc21bc96fd78734c7ee0030976c
igorplopes <igorp.lopes16@gmail.com>
2022-07-18 23:24:34 -0300

    feat: add function to get the blocks that contain the table with the operations

--- src/business/xp_service.py
+++ src/business/xp_service.py
@@ -1,5 +1,12 @@
-from business.pdf_service import extract_text_from_block
+from business.pdf_service import extract_text_from_block, \
+    get_block_index_by_matching_text
 from core.utils import extract_date_from_string
+
+
+def get_table_blocks(blocks):
+    table_start_index = get_block_index_by_matching_text(blocks, 'Negócios realizados')
+    table_end_index = get_block_index_by_matching_text(blocks, 'NOTA DE NEGOCIAÇÃO')
+    return blocks[table_start_index:table_end_index]
 
 
 def is_xp_pdf(pdf):
@@ -10,4 +17,4 @@ def is_xp_pdf(pdf):
 def get_operation_date(blocks):
     date_block = list(
         filter(lambda block: extract_date_from_string(extract_text_from_block(block)) is not None, blocks))
-    return extract_date_from_string(date_block[1])
+    return extract_date_from_string(extract_text_from_block(date_block[1]))
