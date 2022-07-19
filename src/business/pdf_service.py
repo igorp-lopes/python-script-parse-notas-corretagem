60bac53ce47307fd023b6d60ebfb8ae90ef889fd
igorplopes <igorp.lopes16@gmail.com>
2022-07-18 22:16:50 -0300

    feat: add method to get the index of a block using its text

--- src/business/pdf_service.py
+++ src/business/pdf_service.py
@@ -10,8 +10,16 @@ def get_pdf_pages(pdf):
     return (pdf.load_page(i) for i in range(total_pages))
 
 
-def extract_blocks_from_page(page):
-    return page.get_text("blocks", sort=True)
+def extract_blocks_from_page(page, sort=True):
+    return page.get_text("blocks", sort)
+
+
+def get_block_index_by_matching_text(blocks, text_to_match):
+    for index, block in enumerate(blocks):
+        if text_to_match in extract_text_from_block(block):
+            return index
+        
+    return None
 
 
 def extract_text_from_block(block):
