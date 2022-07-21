e103257b01638acd8bb304b0fc580bef5260c4dd
igorplopes <igorp.lopes16@gmail.com>
2022-07-20 21:30:17 -0300

    feat: add function to get the table header info

--- src/business/xp_service.py
+++ src/business/xp_service.py
@@ -1,12 +1,26 @@
+from more_itertools import collapse
+
 from business.pdf_service import extract_text_from_block, \
     get_block_index_by_matching_text
 from core.utils import extract_date_from_string
 
 
-def get_table_blocks(blocks):
-    table_start_index = get_block_index_by_matching_text(blocks, 'Negócios realizados')
-    table_end_index = get_block_index_by_matching_text(blocks, 'NOTA DE NEGOCIAÇÃO')
-    return blocks[table_start_index:table_end_index]
+def get_table_headers(header_block):
+    block_text = extract_text_from_block(header_block)
+    headers = block_text.split('\n')
+
+    headers[2] = headers[2].split(' ', 1)
+    headers[5] = headers[5].split('(*)')
+    headers.pop()
+    headers = list(collapse(headers))
+
+    return headers
+
+
+def get_table_blocks(all_blocks):
+    table_start_index = get_block_index_by_matching_text(all_blocks, 'Negócios realizados')
+    table_end_index = get_block_index_by_matching_text(all_blocks, 'NOTA DE NEGOCIAÇÃO')
+    return all_blocks[table_start_index:table_end_index]
 
 
 def is_xp_pdf(pdf):
