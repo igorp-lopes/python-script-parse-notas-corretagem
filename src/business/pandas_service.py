19d48fcceb967356e048fe25455c4c118c4ba405
Igor Peterossi Lopes <igorp.lopes16@gmail.com>
2022-08-04 00:04:22 -0300

    Feature - GH7: Create a pandas service to map the extracted data to a proper tabular data file (#8)
    * feat: add method to get existing output data csv or create an empty one
    
    * feat: add constant for the name of the output file
    
    * feat: add function to transform a list of data into a dataframe
    
    * feat: add function to save a dataframe as a csv
    
    * feat: add function to merge two dataframes
    
    * feat: add function to process given data to save it as csv
    
    * fix: correct missing df attribution
    
    * fix: import df using all types as string

---
+++ src/business/pandas_service.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+from core.constants import OUTPUT_TABLE_FILENAME
+from core.directory_utils import get_outputs_folder
+
+
+def export_data_to_csv(data):
+    main_df = get_table_file()
+
+    data_to_be_added = pd.DataFrame()
+    for table in data:
+        new_df = map_data_to_df(table)
+        new_df = new_df.astype(str)
+        data_to_be_added = merge_dataframes(data_to_be_added, new_df)
+
+    main_df = merge_dataframes(main_df, data_to_be_added)
+    save_extracted_data_to_csv(main_df)
+
+
+def map_data_to_df(data):
+    df = pd.DataFrame(data)
+    df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+
+    return df
+
+
+def merge_dataframes(df1, df2):
+    if df1.empty:
+        return df2
+
+    if df2.empty:
+        return df1
+
+    return pd.merge(df1, df2)
+
+
+def get_table_file():
+    outputs_folder = get_outputs_folder()
+    output_file_path = outputs_folder + '/' + OUTPUT_TABLE_FILENAME
+    try:
+        df = pd.read_csv(output_file_path, index_col=0).astype(str)
+    except FileNotFoundError:
+        df = pd.DataFrame()
+
+    return df
+
+
+def save_extracted_data_to_csv(df):
+    outputs_folder = get_outputs_folder()
+    output_file_path = outputs_folder + '/' + OUTPUT_TABLE_FILENAME
+    df.to_csv(output_file_path)
--- src/core/constants.py
+++ src/core/constants.py
@@ -1,2 +1,3 @@
 STOCKS_REGEX = r'\w+\d{1,2}'
 XP_POSSIBLE_OBS_COLUMN_VALUES = ['2', '#', '8', 'D', 'F', 'B', 'A', 'C', 'P', 'H', 'X', 'Y', 'L', 'T', 'I']
+OUTPUT_TABLE_FILENAME = 'data.csv'
